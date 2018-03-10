1ae9dc26220340b76f455408667cf6a78cb2c4cc
LB <lukbuc@zoho.com>
2018-03-10 16:15:55 +0100

    add track to canvas

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,40 @@
 var socket;
+var trackWidth = 800;
+var trackHeight = 600;
+var blockSize = 50;
+
+var track = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+							[1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+							[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1],
+							[1,0,0,0,0,0,0,3,2,0,0,0,0,0,0,1],
+							[1,1,0,0,0,0,0,3,2,0,0,0,0,0,1,1],
+							[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
+
+var cvn;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(51);
+	cvn = createCanvas(trackWidth,trackHeight);
+
+	// background(51);
 
 	socket = io.connect('http://localhost:3000');
 
 	socket.on('mouse', newDrawing);
-
 	socket.emit('i_am_ready');
 
 	socket.on('init', (ds) => {
-		ds.forEach( (d) => {
-			noStroke();
-			fill(255);
-			ellipse(d.x,d.y,20,20);
-		});
+		// ds.forEach( (d) => {
+		// 	noStroke();
+		// 	fill(255);
+		// 	ellipse(d.x,d.y,20,20);
+		// });
+		drawBackground();
 	});
 }
 
@@ -37,4 +56,25 @@ function mouseDragged(){
 	noStroke();
 	fill(255);
 	ellipse(mouseX,mouseY,20,20);
+}
+
+function drawBackground() {
+    var styleMap = {
+        0: '#e5e5e5', // race floor
+        1: '#c6c6c6', // border/wall
+        2: '#e0ffe7', // start line
+        3: '#ffe0e0'  // finish line
+    }
+
+    function draw(x,y,t) {
+          fill(color(styleMap[t]));
+					rect(x*blockSize, y*blockSize, blockSize, blockSize);
+          // ctx.strokeRect(x*blockSize, y*blockSize, blockSize, blockSize);
+    }
+
+    for(var row=0; row < trackHeight/blockSize; row++) {
+      for(var col=0; col < trackWidth/blockSize; col++) {
+        draw(row, col, track[row][col]);
+      }
+    }
 }
