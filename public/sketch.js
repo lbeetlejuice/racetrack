22c08f3ed2696195a80c230800d8d41892492866
LB <lukbuc@zoho.com>
2018-03-11 11:23:55 +0100

    add images of rotating cars

--- public/sketch.js
+++ public/sketch.js
@@ -3,19 +3,24 @@ var trackWidth = 800;   // = 16 Zeilen
 var trackHeight = 600;  // = 12 Spalten
 var blockSize = 50;
 var imgs = [];
+var imgscar = [];
+var imgsothercars = [];
 var name;
 var tempPlayerStates;
 var track = [];
 
 function preload() {
   // img = loadImage('img/car1.png');
-  for(var i = 1; i < 6; i++){
-    imgs.push(loadImage("img/car"+i+".png"));
+  for(var i = 0; i < 8; i++){
+    imgscar.push(loadImage("img/owncar/car"+i+".png")); // 0-7 --> My Cars
   }
-  imgs.push(loadImage("img/track.png"));  //5
-  imgs.push(loadImage("img/border.png")); //6
-  imgs.push(loadImage("img/startline.png")); //7
-  imgs.push(loadImage("img/endline.png"));   //8
+  for(var i = 0; i < 8; i++){
+    imgsothercars.push(loadImage("img/othercar/car"+i+".png")); // 0-7 --> Other Player Cars
+  }
+  imgs.push(loadImage("img/track.png"));      // 0 --> Track
+  imgs.push(loadImage("img/border.png"));     // 1 --> Border
+  imgs.push(loadImage("img/startline.png"));  // 2 --> Startline
+  imgs.push(loadImage("img/endline.png"));    // 3 --> Endline
 }
 
 function setup() {
@@ -35,22 +40,43 @@ function update(gameState) {
 }
 
 function drawCars(players) {
-  console.log(players);
   players.forEach( (p) => {
+    var velox = p.vx;
+    var veloy = p.vy;
+    var pictureNumber;
+    console.log(velox,veloy);
+    if(velox >= 0 && veloy == 0){          // 0°
+      pictureNumber = 0;
+    } else if (velox > 0 && veloy > 0) {  // 0 - 90°
+      pictureNumber = 1;
+    } else if (velox == 0 && veloy > 0) { // 90°
+      pictureNumber = 2;
+    } else if (velox < 0 && veloy > 0) {  // 90 - 180°
+      pictureNumber = 3;
+    } else if (velox < 0 && veloy == 0) {  // 180°
+      pictureNumber = 4;
+    } else if (velox < 0 && veloy < 0) {   // 180 - 270°
+      pictureNumber = 5;
+    } else if (velox == 0 && veloy < 0) {   // 270°
+      pictureNumber = 6;
+    } else if (velox > 0 && veloy < 0) {    // 270 - 360°
+      pictureNumber = 7;
+    }
+    console.log(pictureNumber);
     if(p.name == name){
-      image(imgs[1],p.px*blockSize, p.py*blockSize);
+      image(imgscar[pictureNumber],p.px*blockSize, p.py*blockSize);
     } else {
-      image(imgs[0],p.px*blockSize, p.py*blockSize);
+      image(imgsothercars[pictureNumber],p.px*blockSize, p.py*blockSize);
     }
   });
 }
 
 function drawBackground() {
     var styleMap = {
-        0: '5', // race floor   #e5e5e5
-        1: '6', // border/wall  #c6c6c6
-        2: '7', // start line   #e0ffe7
-        3: '8'  // finish line  #ffe0e0
+        0: '0', // race floor   #e5e5e5
+        1: '1', // border/wall  #c6c6c6
+        2: '2', // start line   #e0ffe7
+        3: '3'  // finish line  #ffe0e0
     }
 
     function draw(cal, row, t) {
